044838520b0ae7c08eea1752214405c14198925e
Thomas <thomas__yu@hotmail.com>
2016-01-12 16:33:02 -0800

    addded updated stuff

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>title</title>
+  </head>
+  <body>
+	<?php
+		//echo "hello worlds ";
+		$temp = " Thomas ";
+		echo 'Hi, my name is';
+		echo $temp;
+		$temp= ' geek ';
+		echo "I am a";
+		echo $temp;
+		$temp = 10;
+		echo "m level is ";
+		echo $temp;
+		/**
+		echo "Hi, my name is";
+		echo $temp;
+		$temp = 'geek';
+		echo “I am a”;
+		echo $temp;
+		$temp = 10;
+		echo 'My level is';
+		echo $temp;
+		**/
+		?>
+  </body>
+</html>
